c1542bba835e5ee69d93500f01dbdebf77ff9737
m-triple-m <triplem656@mail.com>
2023-01-09 12:58:51 +0530

    ..

--- frontend/src/components/main/ViewNovel.js
+++ frontend/src/components/main/ViewNovel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import app_config from "../../config";
 
 const ViewNovel = () => {
@@ -7,10 +7,11 @@ const ViewNovel = () => {
   const url = app_config.apiurl;
   const [novelData, setNovelData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const getNovelData = async () => {
     setLoading(true);
-    const res = await fetch(url + '/novel/getbyid/' + id);
+    const res = await fetch(url + "/novel/getbyid/" + id);
     const data = await res.json();
     console.log(data);
     setNovelData(data);
@@ -19,47 +20,79 @@ const ViewNovel = () => {
 
   useEffect(() => {
     getNovelData();
-  }, [])
+  }, []);
+
+  const buyNovel = () => {
+    sessionStorage.setItem('novel', JSON.stringify(novelData));
+    navigate('/user/checkout');
+  }
 
   const displayNovelData = () => {
     if (!loading && novelData) {
-      return <div className="container">
+      return (
         <div className="row">
           <div className="col-md-6">
-            <img className="img-fluid" src={url + "/" + novelData.image}/>
+            <img
+              className="img-fluid w-100"
+              src={url + "/" + novelData.image}
+              alt=""
+            />
           </div>
-          <div className="col">
-            <div className="card">
+          <div className="col-md-6 ">
+            <div className="card h-100">
               <div className="card-body ">
-                <h2>{novelData.title}</h2>
-                <p><i class="fas fa-pen"></i> <b>{novelData.author}</b></p>
-                <p><b>{new Date(novelData.createAt).toLocaleDateString()}</b></p>
-                <p><b>{novelData.genre}</b></p>
-                <p><b>{novelData.publisher}</b></p>
-                <p><b>{novelData.rentable}</b></p>
-                <p><b>{novelData.sellable}</b></p>
-                <p><i class="fas fa-periscope"></i><b>{novelData.rentPrice}</b></p>
-                <p><i class="fas fa-periscope"></i><b>{novelData.sellPrice}</b></p>
-                <p><b>{novelData.user.name}</b></p>
-                <p><b>{novelData.id}</b></p>
+                <h2 className="display-4">{novelData.title}</h2>
+                <p>
+                  <i class="fas fa-pen"></i> <b>{novelData.author}</b>
+                </p>
+                <p>
+                  <b>{new Date(novelData.createAt).toLocaleDateString()}</b>
+                </p>
+                <p>
+                  <b>{novelData.genre}</b>
+                </p>
+                <p>
+                  <b>{novelData.publisher}</b>
+                </p>
+
+                <p>
+                  <i class="fas fa-periscope"></i>
+                  <b>{novelData.rentPrice}</b>
+                </p>
+                <p>
+                  <i class="fas fa-periscope"></i>
+                  <b>{novelData.sellPrice}</b>
+                </p>
+                <p>
+                  <b>{novelData.user.name}</b>
+                </p>
+                <p>
+                  <b>{novelData.id}</b>
+                </p>
+
+                <div className="d-flex">
+                  {!novelData.sellable && (
+                    <button className="btn btn-primary me-4" onClick={buyNovel}>Buy Now</button>
+                  )}
+                  {novelData.rentable && (
+                    <button className="btn btn-danger">Rent Now</button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      );
     }
-  }
-
+  };
 
   return (
     <div className="viewnovel-bg">
-      <div className="col-md-6 mx-auto pt-5">
-        <div className="container">
-          <h1 className="text-center mb-5">ViewNovel</h1>
-    {displayNovelData()}
+      <div className="container">
+        <h1 className="text-center mb-5">ViewNovel</h1>
+        {displayNovelData()}
+      </div>
     </div>
-    </div>
-  </div>
   );
 };
 
