7c3585c30099b766ec7b02e005af7462b6330ceb
kmrinakumari <rinaggp@gmail.com>
2023-01-13 13:11:09 +0530

    ...

--- frontend/src/components/main/ViewNovel.js
+++ frontend/src/components/main/ViewNovel.js
@@ -56,7 +56,7 @@ const ViewNovel = () => {
                     <button className="btn btn-primary me-4" onClick={buyNovel}>Buy Now</button>
                   )}
                   {novelData.rentable && (
-                    <button className="btn btn-danger" >Rent Now</button>
+                    <button className="btn btn-danger"  onClick={buyNovel}>Rent Now</button>
                   )}
                 </div>
               </div>
